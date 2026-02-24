1376fad64f11f0dbd50990fc487fbd97e6659ade
min_pooneh <poonehebrahimi035@gmail.com>
2026-02-24 11:07:35 +0330

    feat :add hover to themes

--- src/components/theme-picker.tsx
+++ src/components/theme-picker.tsx
@@ -1,14 +1,29 @@
 import { useTheme } from "next-themes";
 
+import base from "../assets/base.png";
+import green from "../assets/green.png";
+import black from "../assets/black.png";
+import blue from "../assets/blue.png";
+import red from "../assets/red.png";
+import orange from "../assets/orange.png";
+import pink from "../assets/pink.png";
+import purple from "../assets/purple.png";
+
+import {
+  HoverCard,
+  HoverCardContent,
+  HoverCardTrigger,
+} from "@/components/ui/hover-card";
+
 const themeColors = {
-  base: "#fff",
-  green: "#0f0",
-  blue: "#00f",
-  red: "#f00",
-  orange: "#f27d07",
-  pink: "#faa",
-  purple: "#961d80",
-  gray: "#5c5858",
+  base: { color: "#fff", img: base },
+  green: { color: "#0f0", img: green },
+  blue: { color: "#00f", img: blue },
+  red: { color: "#f00", img: red },
+  orange: { color: "#f27d07", img: orange },
+  pink: { color: "#faa", img: pink },
+  purple: { color: "#961d80", img: purple },
+  gray: { color: "#5c5858", img: black },
 };
 
 export const THEMES = Object.keys(themeColors);
@@ -21,17 +36,28 @@ export default function ThemePicker() {
       <span className="text-text text-2xl font-bold">Themes</span>
       <div className="grid grid-cols-4 gap-3">
         {THEMES.map((theme, index) => (
-          <div
-            className="p-7 rounded-full "
-            key={index}
-            style={{
-              backgroundColor: themeColors[theme as keyof typeof themeColors],
-            }}
-            onClick={() => setTheme(theme)}
-          />
+          <HoverCard>
+            <HoverCardTrigger>
+              <div
+                className="p-7 rounded-full "
+                key={index}
+                style={{
+                  backgroundColor:
+                    themeColors[theme as keyof typeof themeColors].color,
+                }}
+                onClick={() => setTheme(theme)}
+              />
+            </HoverCardTrigger>
+            <HoverCardContent className="w-100">
+              <img
+                src={themeColors[theme as keyof typeof themeColors].img}
+                alt="gheme color screenshot"
+                className="w-full"
+              />
+            </HoverCardContent>
+          </HoverCard>
         ))}
       </div>
     </div>
   );
 }
-
