66817fc827431c2b064b90b1a35221db0150766c
p.ebrahimi <p.ebrahimi@darvagcloud.com>
2026-06-01 16:20:38 +0330

    fix:change search ui

--- src/components/search.tsx
+++ src/components/search.tsx
@@ -21,26 +21,29 @@ export default function Search() {
   });
 
   return (
-    <div className="relative w-full">
-      <SearchIcon className="absolute right-2 top-1/115 text-text" />
-      <input
-        className="bg-gray-300/50 border-text py-2 px-200 hover:ring-10 ring-primary w-full text-text"
-        value={search}
-        onChange={handleChange}
-      />
-      {data ? (
-        <div className="absolute z-100 flex flex-col py-2 bg-primary w-full px-2 divide-y divide-gray-300">
+    <div>
+      <div className="flex flex-col text-6xl text-text gap-1 p-10 px-40">
+        <span className="text-4xl px-2 text-white">Search</span>
+        <span className="text-xl px-2 text-gray-300">
+          Find your favorite artist
+        </span>
+      </div>
+      <div className="items-center justify-center flex gap-2">
+        <SearchIcon className="absolute text-amber-300 py-3" />
+        <input
+          className="flex items-center justify-center border rounded-sm bg-gray-300/50 border-text py-2 hover:ring-10 ring-primary w-328 text-text px-2"
+          value={search}
+          onChange={handleChange}
+        />
+      </div>
+      {data && (
+        <div className="absolute z-100 flex flex-col py-2 bg-primary justify-center w-358 divide-y divide-gray-300">
           {data.results.map((song) => (
             <SearchResult song={song} key={song.id} />
           ))}
           {data.results.length === 0 && (
             <span className="text-text">Song not found :(</span>
           )}
-        </div>
-      ) : (
-        <div className="flex flex-col text-6xl text-text items-center justify-center h-screen gap-2">
-          <SearchIcon size={200} />
-          Search..!
         </div>
       )}
 
